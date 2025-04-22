6794476d8dc0fc839a030bd9a46d1f4d02407f0a
SIW2O25 <sheltie37@naver.com>
2025-04-22 21:07:51 +0900

    update : 간격 수정

--- GameManager.cs
+++ GameManager.cs
@@ -16,10 +16,10 @@
             player.SetPlayerInfo(15, "보리스", "검사", 35, 26, 450, 50000);
 
             Inventory inventory = new Inventory();
-            inventory.SetInventoryInfo("[E]등급(노멀) | 내구도 30/100 | 공격력 +5 | 데미지 8~16", "등급(노멀) | 내구도 80/100 | 방어력 +15", "등급(레어) | 내구도 100/100 | 방어력 + 35", "100 | 회복력", "40 | 회복력");
+            inventory.SetInventoryInfo("[Sell]등급(노멀) | 내구도 30/100 | 공격력 +5 | 데미지 8~16", "[Sell]등급(노멀) | 내구도 80/100 | 방어력 +15", "[Sell]등급(레어) | 내구도 100/100 | 방어력 + 35", "100 | 회복력", "40 | 회복력");
 
             Shop shop = new Shop();
-            shop.SetShopInfo("\\(소)\n\t1000 G", "\\(소)\n\t1200G ", "\\방어력 +5\n\t500G", "\\공격력 +3\\데미지 1~6\n\t800G");
+            shop.SetShopInfo("(소) | 1000 G", "(소) | 1200G ", "방어력 +5 | 500G", "공격력 +3 | 데미지 1~6 | 800G");
 
             Menu menu = new Menu();
             menu.SetPlayer(player);
